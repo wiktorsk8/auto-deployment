2a05ac68747afe0e0f68f37d7fb1a17b071f4a00
wiktor <kowalinski.wiktor04@gmail.com>
2024-12-26 18:32:00 +0100

    fix path in run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 SCRIPT_PATH=$(realpath "$0")
+SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
 
-source "$SCRIPT_PATH"/env-setup.sh
-"$SCRIPT_PATH"/pull-changes.sh
+source "$SCRIPT_DIR"/env-setup.sh
+"$SCRIPT_DIR"/pull-changes.sh
